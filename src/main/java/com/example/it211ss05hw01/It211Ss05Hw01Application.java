0b5220bd9e2af50c7012ed25bf52e1a827c2094a
TMon283 <lemonboy2k6@gmail.com>
2026-05-21 00:17:04 +0700

    up

---
+++ src/main/java/com/example/it211ss05hw01/It211Ss05Hw01Application.java
@@ -0,0 +1,13 @@
+package com.example.it211ss05hw01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It211Ss05Hw01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It211Ss05Hw01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/it211ss05hw01/It211Ss05Hw01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it211ss05hw01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It211Ss05Hw01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
